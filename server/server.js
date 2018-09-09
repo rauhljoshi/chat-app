02b61baf184fabb0a775c4b1be74be5fb295c325
Rahul Joshi <jimi.rahul@gmail.com>
2018-09-09 09:19:30 +0530

    configuring socket io on client and server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
-const app = express();
 const publicPath = path.join(__dirname, '../public');
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	socket.on('disconnect', () => {
+		console.log('User was disconnected');
+	});
+});
 
 // app.get('/', (req, res) => {
 // 	res.render('index.html');
 // });
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`listening on port ${port}`);
 });
