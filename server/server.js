ffccd333c697e1de6f71ac85223dc4e874152d10
Rahul Joshi <jimi.rahul@gmail.com>
2018-09-10 09:23:06 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat room',
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New User joined',
+		createdAt: new Date().getTime()
+	});
+
 	socket.on('createMessage', (msg) => {
 		console.log('createMessage', msg);
 		io.emit('newMessage', {
@@ -21,6 +33,12 @@ io.on('connection', (socket) => {
 			text: msg.text,
 			createdAt: new Date().getTime()
 		});
+
+		// socket.broadcast.emit('newMessage', {
+		//  	from: msg.from,
+		//  	text: msg.text,
+		// 	createdAt: new Date().getTime()
+		// });
 	});
 
 	socket.on('disconnect', () => {
